2835ccd2d22576a208f96f4601eaf7dd433387fe
Leonardo Oliveira <lnrdoliveira.dev@gmail.com>
2024-03-11 17:49:22 -0300

    aulas

---
+++ bloco_2/aula37.py
@@ -0,0 +1,10 @@
+# Exemplo de uso dos sets
+letras = set()
+
+while True:
+    letra = input('Digite: ')
+    letras.add(letra.lower())
+    if 'l' in letras:
+        print('parabains')
+        break
+    print(letras)
