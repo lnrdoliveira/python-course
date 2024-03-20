c7e8247b50dd982bad5cd8c1221badbb80f9092e
Leonardo Oliveira <lnrdoliveira.dev@gmail.com>
2024-03-20 14:17:40 -0300

    aulas

--- bloco_2/aula43.py
+++ bloco_2/aula43.py
@@ -1,3 +1,28 @@
 # Introdução à List comprehension em Python
 # List comprehension é uma forma rápida para criar listas
 # a partir de iteráveis.
+
+lista = []
+for numero in range(10):
+    lista.append(numero)
+
+lista = [
+    numero * 2
+    for numero  in range(10)
+]
+print(list(range(10)))
+print(lista)
+
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 10, },
+    {'nome': 'p2', 'preco': 20, },
+    {'nome': 'p3', 'preco': 30, },
+]
+novos_produtos = [
+    {**produto, 'preco': produto['preco']*1.05} #alterando a chave preco, aumento de 5% em cada produto
+    if produto['preco'] > 20 else {**produto} # if ternario para aumentar o preco somente em precos maiores que 20
+    for produto in produtos
+]
+print(novos_produtos)
+print(*novos_produtos, sep='\n')
