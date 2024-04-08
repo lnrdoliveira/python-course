0e8395b5818176b7b5af344470af9599c9524741
Leonardo Oliveira <lnrdoliveira.dev@gmail.com>
2024-04-08 08:49:35 -0300

    aulas

---
+++ bloco_2/aula48.py
@@ -0,0 +1,8 @@
+"""
+dir, hasattr e getattr em python
+"""
+string = 'Luiz'
+
+if hasattr(string, 'upper'):
+    print('tem upper')
+    print(string.upper())
---
+++ bloco_2/aula49.py
@@ -0,0 +1,13 @@
+"""
+Generator expression, iterables e iterators em python
+"""
+import sys
+
+iterable = ['Eu', 'Tenho', '__iter__']
+iterator = iter(iterable) # tem __iter__ e __next__
+lista = [n for n in range(10)]
+generator = (n for n in range(10))
+print(lista)
+print(generator)
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
